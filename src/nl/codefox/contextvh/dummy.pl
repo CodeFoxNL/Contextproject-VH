ca101c18efc1e01d2428a5e84a1558739a9f7acf
MHMCD <MHMCD@users.noreply.github.com>
2016-05-20 23:34:44 +0200

    Added bot with some extra percepts for items we have been working on

--- src/nl/codefox/contextvh/dummy.pl
+++ src/nl/codefox/contextvh/dummy.pl
@@ -3,6 +3,8 @@
 :- dynamic settings/1.
 :- dynamic functions/1.
 :- dynamic buildings/1.
+:- dynamic zone_links/1.
+:- dynamic upgrade_types/1.
 
 % we have a building if the building list has at least 1 element.
 havebuilding :- buildings([X|Y]).
