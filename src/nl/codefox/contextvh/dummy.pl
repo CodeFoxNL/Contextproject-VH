b922a6971e893af205aacc9b9a7094b59e6511cd
lisv <lisette_stella@hotmail.com>
2016-06-01 10:00:36 +0200

    Adapt knowledgebase and fix some events to fit updated connector

--- src/nl/codefox/contextvh/dummy.pl
+++ src/nl/codefox/contextvh/dummy.pl
@@ -1,5 +1,3 @@
-
-
 :- dynamic stakeholders/1.
 :- dynamic settings/1.
 :- dynamic functions/1.
@@ -10,7 +8,6 @@
 :- dynamic offeredLand/0.
 :- dynamic soldLand/0.
 :- dynamic lands/1.
-
 
 :- dynamic indicator/3.
 :- dynamic indicatorLink/2.
