dd18fa91063166ce53f64527c255c36a909e2122
lisv <lisette_stella@hotmail.com>
2016-06-02 13:20:34 +0200

    Add modules that the agent can use (not finished yet).

--- src/nl/codefox/contextvh/knowledgebase.pl
+++ src/nl/codefox/contextvh/knowledgebase.pl
@@ -20,7 +20,6 @@
 :- dynamic zone_link/4.
 :- dynamic zone/5.
 
-
 %%% Percepts for indicators that the agent uses
 % check with metrics what our agent needs (!)
 % .....
@@ -28,19 +27,23 @@
 %%% Knowledge
 % wanneer is iets gesloopt, gebouwd, gekocht, lege grond
 :- dynamic demolished/1.
-% constructed, planned, bid
+:- dynamic improvedZone/1.
 
-% demolish a building
-demolishBuilding(ID) :- demolished(ID).
+% Goals that can be adopted
+demolishBuilding(BuildingID) :- demolished(BuildingID).
+improveZone(IndicatorID, ZoneID) :- improvedZone(IndicatorID, ZoneID).
 
 % we have a building if the building list has at least 1 element.
-havebuilding :- buildings([X|Y]).
+%havebuilding :- buildings([X|Y]).
 
 % the money we have in our cashstack
 budget(X) :- stakeholder(StakeholderID, 'Private_Woningbouw_Burgers', X, Income).
 
+% check if we own a building
 ownedBuilding(BuildingID) :- stakeholder(StakeholderID, 'Private_Woningbouw_Burgers',_,_), building(_,_,OwnerID,_,_,_,_), OwnerID = StakeholderID. 
 
+% determine when a zone needs to be improved and on what aspect
 needImprovement(IndicatorID, ZoneID) :- indicator(Id, Value, Target, ZoneLink), member(zone_link(ZoneID,IndicatorID,CurrentValue,CurrentTarget), ZoneLink), CurrentValue < CurrentTarget.  
+% a zone is improved when the indicator score is higher or equal to the agent's target
 improvedZone(IndicatorID, ZoneID) :- indicator(Id, Value, Target, ZoneLink), member(zone_link(ZoneID,IndicatorID,CurrentValue,CurrentTarget), ZoneLink), CurrentValue >= CurrentTarget.
 
