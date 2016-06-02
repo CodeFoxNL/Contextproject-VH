0239b607a6a1d161af125cabb9d00a5a7d422dc3
lisv <lisette_stella@hotmail.com>
2016-06-02 16:30:14 +0200

    Add myBuildingList to have easy access to all the buildings that we own

--- src/nl/codefox/contextvh/knowledgebase.pl
+++ src/nl/codefox/contextvh/knowledgebase.pl
@@ -1,46 +1,46 @@
 %%% Percepts that the agent receives from the environment
-:- dynamic offeredLand/0.
-:- dynamic soldLand/0.
-
 :- dynamic stakeholders/1.
 :- dynamic settings/1.
 :- dynamic functions/1.
 :- dynamic indicators/1.
 :- dynamic buildings/1.
-:- dynamic upgrade_types/1.
 :- dynamic lands/1.
 :- dynamic zones/1.
 :- dynamic zone_links/1.
 :- dynamic requests/1.
+:- dynamic actions/1.
 
+% others
+:- dynamic offeredLand/0.
+:- dynamic soldLand/0.
+:- dynamic upgrade_types/1.
 :- dynamic indicatorLink/2.
 :- dynamic indicator/4.
 :- dynamic stakeholder/4.
-:- dynamic building/7.
 :- dynamic zone_link/4.
+:- dynamic building/7.
 :- dynamic zone/5.
 
-%%% Percepts for indicators that the agent uses
-% check with metrics what our agent needs (!)
-% .....
-
 %%% Knowledge
-% wanneer is iets gesloopt, gebouwd, gekocht, lege grond
 :- dynamic demolished/1.
 :- dynamic improvedZone/1.
+:- dynamic myBuildingList/1.
 
 % Goals that can be adopted
 demolishBuilding(BuildingID) :- demolished(BuildingID).
 improveZone(IndicatorID, ZoneID) :- improvedZone(IndicatorID, ZoneID).
 
-% we have a building if the building list has at least 1 element.
-%havebuilding :- buildings([X|Y]).
-
+% the ID of our agent
+myStakeholderID(StakeholderID) :- stakeholder(StakeholderID, 'Private_Woningbouw_Burgers', Money, Income).
 % the money we have in our cashstack
-budget(X) :- stakeholder(StakeholderID, 'Private_Woningbouw_Burgers', X, Income).
+budget(Money) :- stakeholder(StakeholderID, 'Private_Woningbouw_Burgers', Money, Income).
 
-% check if we own a building
-ownedBuilding(BuildingID) :- stakeholder(StakeholderID, 'Private_Woningbouw_Burgers',_,_), building(_,_,OwnerID,_,_,_,_), OwnerID = StakeholderID. 
+% check if our agent owns a building
+ownedBuilding(BuildingID) :- building(BuildingID,_,OwnerID,_,_,_,_), OwnerID = myStakeholderID(StakeholderID). 
+% check if a building is ours
+mine(building(_,_,myStakeholderID(StakeholderID),_,_,_,_,_)).
+% generates list of all buildings that the agent owns
+myBuildings(MyBuildings, List):- findall(Building, (member(Building, List), mine(Building)), Members), sort(Members, MyBuildings).
 
 % determine when a zone needs to be improved and on what aspect
 needImprovement(IndicatorID, ZoneID) :- indicator(Id, Value, Target, ZoneLink), member(zone_link(ZoneID,IndicatorID,CurrentValue,CurrentTarget), ZoneLink), CurrentValue < CurrentTarget.  
