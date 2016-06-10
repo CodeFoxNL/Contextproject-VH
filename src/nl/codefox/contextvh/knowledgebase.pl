4550fd85527affa02517aa277d6d93cbc7a5e61d
lisv <lisette_stella@hotmail.com>
2016-06-10 12:26:55 +0200

    Adjust quality and sound indicator definitions

--- src/nl/codefox/contextvh/knowledgebase.pl
+++ src/nl/codefox/contextvh/knowledgebase.pl
@@ -66,11 +66,13 @@ needImprovement(IndicatorID, ZoneID) :- indicator(IndicatorID, Value, Target, Zo
 improvedZone(IndicatorID, ZoneID) :- indicator(IndicatorID, Value, Target, ZoneLink), member(zone_link(ZoneID,IndicatorID,CurrentValue,CurrentTarget), ZoneLink), CurrentValue >= CurrentTarget.
 
 % indicator of the spatial quality
-qualityIndicator(Value, Target, ZoneLink) :- 
+qualityIndicator(Value, Target, ZoneID) :- 
 	indicatorLink(_, IndicatorWeights), member(indicatorWeights(IndicatorId, IndicatorName, _), IndicatorWeights), 
-	(IndicatorName == 'Ruimtelijke kwaliteit'), indicator(IndicatorId, Value, Target, ZoneLink).
+	(IndicatorName == 'Ruimtelijke kwaliteit'), indicator(IndicatorId, Value, Target, ZoneLink), 
+	member(zone_link(ZoneID, IndicatorID, Value, Target), ZoneLink).
 
 % indicator of the sound
-soundIndicator(Value, Target, ZoneLink) :- 
+soundIndicator(Value, Target, ZoneID) :- 
 	indicatorLink(_, IndicatorWeights), member(indicatorWeights(IndicatorId, IndicatorName, _), IndicatorWeights), 
-	(IndicatorName == 'Geluidsoverlast Verkeer'), indicator(IndicatorId, Value, Target, ZoneLink).
+	(IndicatorName == 'Geluidsoverlast Verkeer'), indicator(IndicatorId, Value, Target, ZoneLink), 
+	member(zone_link(ZoneID, IndicatorID, Value, Target), ZoneLink).
