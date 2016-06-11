bb594f1062f3816dd0fab97b48f0e74a73018bc3
lisv <lisette_stella@hotmail.com>
2016-06-11 20:26:15 +0200

    Adjust some things to have more consistent and clear code

--- src/nl/codefox/contextvh/knowledgebase.pl
+++ src/nl/codefox/contextvh/knowledgebase.pl
@@ -34,32 +34,36 @@
 %%% Goals that can be adopted
 demolishBuilding(BuildingID) :- demolished(BuildingID).
 constructBuilding(BuildingID) :- constructed(BuildingID).
-upgradeBuilding(UpgradeID, MultiPolygon) :- upgraded(MultiPolygon).
-improveZone(IndicatorID, ZoneID) :- improvedZone(IndicatorID, ZoneID).
+upgradeBuilding(UpgradeID,MultiPolygon) :- upgraded(MultiPolygon).
+improveZone(IndicatorID,ZoneID) :- improvedZone(IndicatorID,ZoneID).
 % we achieve these goals when we have tried 3 times or when we actually bought or sold land.
-doneBuying(MultiPoly) :- (requestedLand(MultiPoly, Counter), Counter >= 3) ; ourLand(MultiPoly).
-doneSelling(MultiPoly) :- (offeredLand(MultiPoly, Counter), Counter >= 3); landOfOthers(MultiPoly).
+doneBuying(MultiPoly) :- (requestedLand(MultiPoly, Counter), Counter>=3); ourLand(MultiPoly).
+doneSelling(MultiPoly) :- (offeredLand(MultiPoly, Counter), Counter>=3); landOfOthers(MultiPoly).
 
 % determine when a zone needs to be improved and for which indicator
-needImprovement(IndicatorID, ZoneID) :- indicator(IndicatorID, Value, Target, ZoneLink), member(zone_link(ZoneID,IndicatorID,CurrentValue,CurrentTarget), ZoneLink), CurrentValue < CurrentTarget.  
+needImprovement(IndicatorID,ZoneID) :- indicator(IndicatorID,Value,Target,ZoneLink), member(zone_link(ZoneID,IndicatorID,CurrentValue,CurrentTarget),ZoneLink), CurrentValue<CurrentTarget.  
 % a zone is improved when the indicator score is higher or equal to the agent's target
-improvedZone(IndicatorID, ZoneID) :- indicator(IndicatorID, Value, Target, ZoneLink), member(zone_link(ZoneID,IndicatorID,CurrentValue,CurrentTarget), ZoneLink), CurrentValue >= CurrentTarget.
+improvedZone(IndicatorID,ZoneID) :- indicator(IndicatorID,Value,Target,ZoneLink), member(zone_link(ZoneID,IndicatorID,CurrentValue,CurrentTarget),ZoneLink), CurrentValue>=CurrentTarget.
 
 %%% Checks
 % check if our agent owns a building
-ownedBuilding(BuildingID) :- building(BuildingID,_,OwnerID,_,_,_,_,_), stakeholder(StakeholderID, 'Private_Woningbouw_Burgers', Money, Income), StakeholderID = OwnerID. 
+ownedBuilding(BuildingID) :- building(BuildingID,_,OwnerID,_,_,_,_,_), stakeholder(StakeholderID,'Private_Woningbouw_Burgers',Money,Income),StakeholderID=OwnerID. 
 % budget of our Stakeholder
-budget(Amount) :- stakeholder(StakeholderID, 'Private_Woningbouw_Burgers', Amount, Income).
+budget(Amount) :- stakeholder(StakeholderID,'Private_Woningbouw_Burgers',Amount,Income).
 % stakeholder ID for our Stakeholder
-ourID(StakeholderID) :- stakeholder(StakeholderID, 'Private_Woningbouw_Burgers', Money, Income).
+ourID(StakeholderID) :- stakeholder(StakeholderID,'Private_Woningbouw_Burgers',Money,Income).
 % stakeholder IDs for other stakeholders
-notOurID(StakeholderID) :- stakeholder(StakeholderID, _, _, _), not(ourID(StakeholderID)).
+notOurID(StakeholderID) :- stakeholder(StakeholderID,_,_,_), not(ourID(StakeholderID)).
 % predicates to determine if land on the map is ours or others
-landOfOthers(MultiPoly) :- lands(List), stakeholder(StakeholderID, 'Private_Woningbouw_Burgers', _, _), not(member(land(LandID, stakeholder(StakeholderID,_,_,_), MultiPoly, _, _), List)), MultiPoly \= multipolygon('MULTIPOLYGON EMPTY').
-ourLand(MultiPoly) :- lands(List), stakeholder(StakeholderID, 'Private_Woningbouw_Burgers', _, _), member(land(LandID, stakeholder(StakeholderID,_,_,_), MultiPoly, _, _), List), MultiPoly \= multipolygon('MULTIPOLYGON EMPTY').
+landOfOthers(MultiPoly) :- lands(List), stakeholder(StakeholderID,'Private_Woningbouw_Burgers',_,_),
+	not(member(land(LandID,stakeholder(StakeholderID,_,_,_),MultiPoly,_,_),List)),
+	MultiPoly\=multipolygon('MULTIPOLYGON EMPTY').
+ourLand(MultiPoly) :- lands(List), stakeholder(StakeholderID,'Private_Woningbouw_Burgers',_,_),
+	member(land(LandID,stakeholder(StakeholderID,_,_,_),MultiPoly,_,_),List),
+	MultiPoly\=multipolygon('MULTIPOLYGON EMPTY').
 
 % uses indicator scores to determine whether more Luxury houses are desirable, if this is not the case Normal houses are desirable.  
-needLuxeHouse:- indicator(34,_, _, ZoneLinkList), member(zone_link(0,_,Current1,Target1), ZoneLinkList), 
-indicator(34,_, _, ZoneLinkList), member(zone_link(1,_,Current2,Target2), ZoneLinkList),
-Target1 - Current1 > Target2 - Current2.
+needLuxeHouse:- indicator(34,_, _, ZoneLinkList), member(zone_link(0,_,Current1,Target1),ZoneLinkList), 
+indicator(34,_,_,ZoneLinkList), member(zone_link(1,_,Current2,Target2),ZoneLinkList),
+Target1-Current1>Target2-Current2.
 
